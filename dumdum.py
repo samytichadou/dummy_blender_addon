567d8ec10c5ed200241c64a4e270f5004022c687
samytichadou <samytichadou@gmail.com>
2021-12-07 01:05:49 +0100

    Add files via upload

--- dumdum.py
+++ dumdum.py
@@ -10,25 +10,32 @@ bl_info = {
     "category": "Development",
 }
 
-
 import bpy
 
+# file menu
+class DUMDUM_MT_menu(bpy.types.Menu):
+    bl_label = "Dumdum"
+    def draw(self, context):
+        layout = self.layout
+        layout.label(text='Dumdum1')
+        layout.separator()
+        layout.label(text='Dumdum2')
+        layout.separator()
+        layout.operator('world.new', text="Create World", icon="WORLD")
 
 # file menu drawer
 def topbar_header(self, context):
     if context.region.alignment == 'RIGHT':
-        layout = self.layout
-        self.layout.label(text='Dumdum')
-
+        self.layout.menu('DUMDUM_MT_menu')
 
 # Registration
 def register():
+    bpy.utils.register_class(DUMDUM_MT_menu)
     bpy.types.TOPBAR_HT_upper_bar.prepend(topbar_header)
 
-
 def unregister():
+    bpy.utils.unregister_class(DUMDUM_MT_menu)
     bpy.types.TOPBAR_HT_upper_bar.remove(topbar_header)
-
 
 if __name__ == "__main__":
     register()
